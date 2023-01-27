f9b68152c09e07986e61449087fa14aa4668c509
Ezreal <2622285081@qq.comgit config --global user.email 2622285081@qq.com>
2023-01-27 16:30:40 +0800

    idea hot-fix 1

--- src/main/java/gitTest.java
+++ src/main/java/gitTest.java
@@ -2,5 +2,6 @@ public class gitTest {
     public static void main(String[] args) {
         System.out.println(":444");
         System.out.println(":554");
+        System.out.println(":hot");
     }
 }
